e1e1a96698baa481ef73edebfbab728108252291
Muhammad SA <MuhammadSA211999>
2023-07-28 16:21:06 +0600

    Expained object in dsa

--- stringMethod.js
+++ stringMethod.js
@@ -1,1 +1,10 @@
-// Explored Javascript  all String-Object method:
+// Explored Javascript  all usually used String-Object method:
+
+// Object: An object is a self contained entity that containes **data and **methods. The data of an object are called it's **State and the **Methods are calledt it's **beheviour.
+// Bye the methods of an object we can create a meaningfull interactivity with the objects property.
+// Object can be used to represents a real world entity that can stored in a computer. They are powerfull toll for data organization and manipulating.
+
+// ** String is a iterable object in JavaScript.
+
+let text = 'My name is MSA. Im a dedicated and determined Full Stack application developer.'
+
